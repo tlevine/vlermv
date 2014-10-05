26882a46faaab9c713d2f54b9ead4db68036c8e3
Thomas Levine <_@thomaslevine.com>
2014-10-05 21:10:08 +0000

    setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(name='vlermv',
+      author='Thomas Levine',
+      author_email='_@thomaslevine.com',
+      description='Easily dump python objects to files, and then load them back.',
+      url='https://github.com/tlevine/vlermv',
+#     packages=['pickle_warehouse'],
+      install_requires = [],
+      tests_require = ['nose'],
+      version='0.2',
+      license='AGPL',
+)
