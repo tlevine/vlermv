8daefadbde8d182253cca25ce371666523bf3d05
Thomas Levine <_@thomaslevine.com>
2014-10-26 11:47:45 +0000

    names

--- vlermv/cache.py
+++ vlermv/cache.py
@@ -11,19 +11,19 @@ class CachedFunction(Vlermv):
 
 def cache(*args, **kwargs):
     '''
-    Cache a function with a pickle_warehouse.Warehouse.
+    Cache a function with a vlermv.Vlermv.
 
     Decorate the function with @cache(*args, **kwargs).
-    The args and kwargs get passed to the Warehouse.
+    The args and kwargs get passed to the Vlermv.
     For example::
 
         @cache('~/.http')
         def get(url):
             return requests.get(url, auth = ('username', 'password'))
 
-    pickle_warehouse.Warehouse would ordinarily fail if
+    vlermv.Vlermv would ordinarily fail if
     no arguments were passed to it. If you pass no arguments
-    to cache, the Warehouse directory argument (the one
+    to cache, the Vlermv directory argument (the one
     required argument) will be set to the name of the function.
     '''
 
