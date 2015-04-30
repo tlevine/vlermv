3de018aebe40d399560eb1090130e9bada96c18a
Thomas Levine <_@thomaslevine.com>
2015-04-30 07:47:01 +0000

    test_exceptions

--- vlermv/test/test_exceptions.py
+++ vlermv/test/test_exceptions.py
@@ -9,12 +9,11 @@ def test_open_error():
         open('/not-a-file')
 
 def test_permission_error():
-    with tempfile.NamedTemporaryFile() as tmp:
-        tmp.write('')
+    tmp = tempfile.NamedTemporaryFile()
     os.chmod(tmp.name, 000)
     with pytest.raises(exceptions.PermissionError):
         open(tmp.name, 'w').write('stuff')
-    os.remomve(tmp.name)
+    os.remove(tmp.name)
 
 def test_delete_error():
     with pytest.raises(exceptions.DeleteError):
@@ -24,6 +23,6 @@ def test_file_exists_error():
     with pytest.raises(exceptions.FileExistsError):
         os.mkdir('/')
 
-def test_out_of_space(exception):
-    import warnings
-    warnings.warn('out_of_space is not tested.')
+@pytest.mark.skipif(True, reason = 'Running out of space in a test is annoying.')
+def test_out_of_space():
+    raise NotImplementedError
