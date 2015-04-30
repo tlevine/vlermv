b06dc85477280719d94dcd268038dd65ed808a5f
Thomas Levine <_@thomaslevine.com>
2015-04-30 07:34:31 +0000

    exception tests

---
+++ vlermv/test/test_exceptions.py
@@ -0,0 +1,25 @@
+import os
+
+import pytest
+
+from .. import exceptions
+
+def test_open_error():
+    with pytest.raises(exceptions.OpenError):
+        open('/not-a-file')
+
+def test_permission_error():
+    with tempfile.NamedTemporaryFile() as tmp:
+        tmp.write('')
+    os.chmod(tmp.name, 000)
+    with pytest.raises(exceptions.PermissionError):
+        open(tmp.name, 'w').write('stuff')
+    os.remomve(tmp.name)
+
+def test_delete_error():
+    with pytest.raises(exceptions.DeleteError):
+        os.remove('/not-a-file')
+
+def test_file_exists_error():
+    with pytest.raises(exceptions.FileExistsError):
+        os.mkdir('/')
