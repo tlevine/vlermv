46dab103a05d95febf5113e135266819889f5ed1
Thomas Levine <_@thomaslevine.com>
2015-04-30 08:32:43 +0000

    staticmethod

--- vlermv/serializers/identity.py
+++ vlermv/serializers/identity.py
@@ -1,10 +1,16 @@
 class _identity:
     'Dump and load things that are already serialized.'
-    dump = lambda obj, fp: fp.write(obj)
-    load = lambda fp: fp.read()
+    @staticmethod
+    def dump(obj, fp):
+        fp.write(obj)
+
+    @staticmethod
+    def load(fp):
+        return fp.read()
+
     vlermv_cache_exceptions = False
     def __init__(self, binary_mode = False):
         vlermv_binary_mode = binary_mode
 
 identity_str = _identity(binary_mode = False)
-identity_bytes = _identity(binary_mode = False)
+identity_bytes = _identity(binary_mode = True)
