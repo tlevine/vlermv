1f9da0345caa451e54bf2e422a904c94a7988757
Thomas Levine <_@thomaslevine.com>
2015-04-30 12:39:45 +0000

    error on empty

--- vlermv/util.py
+++ vlermv/util.py
@@ -2,6 +2,9 @@ import os
 
 def split(path):
     'Split a path into a tuple of all directories and then the final directory/file.'
+    if path == '':
+        raise ValueError('Path may not be empty.')
+
     dn, fn = os.path.split(path)
     if dn == '':
         return path
