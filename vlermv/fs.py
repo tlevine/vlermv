75b51cef4bf47eee390b6aee76819c28841ad9ff
Thomas Levine <_@thomaslevine.com>
2015-04-30 07:59:33 +0000

    fs tests

--- vlermv/fs.py
+++ vlermv/fs.py
@@ -2,18 +2,18 @@ import os
 from random import randint
 from string import ascii_letters
 
-def mktemp(tempdir):
+def _random_file_name():
+    n = len(ascii_letters) - 1
+    return ''.join(ascii_letters[randint(0, n)] for _ in range(10))
+
+def mktemp(tempdir, filename = _random_file_name):
     try:
         os.makedirs(tempdir, exist_ok = True)
     except TypeError:
         # Python 2
         if not os.path.isdir(tempdir):
             os.makedirs(tempdir)
-    return os.path.join(tempdir, _random_file_name())
-
-def _random_file_name():
-    n = len(ascii_letters) - 1
-    return ''.join(ascii_letters[randint(0, n)] for _ in range(10))
+    return os.path.join(tempdir, filename())
 
 def _reversed_directories(outer, inner):
     while outer != inner:
---
+++ vlermv/test/test_fs.py
@@ -0,0 +1,22 @@
+import os
+from random import seed
+from tempfile import mkdtemp
+
+import pytest
+
+from .. import fs
+
+def test_mktemp():
+    outer = mkdtemp()
+    inner = os.path.join(outer, '1', '2', '3')
+    filename = fs.mktemp(inner, filename = lambda: '4')
+
+    assert os.path.isdir(inner), 'Temporary directory was not created properly.'
+    assert filename == os.path.join(inner, '4'), 'Temporary file is named incorrectly.'
+    assert not os.path.isfile(filename), 'Temporary file was created; it should not be created.'
+
+def test_random_file_name():
+    seed(1234534324)
+    assert fs._random_file_name() == 'dZTruUBGba'
+
+def test_reversed_directories():
