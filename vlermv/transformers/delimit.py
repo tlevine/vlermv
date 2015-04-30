eb76fefbd7698f7f85d944d298b88e8e827896c6
Thomas Levine <_@thomaslevine.com>
2015-04-30 12:59:04 +0000

    testing transformers

--- vlermv/transformers/delimit.py
+++ vlermv/transformers/delimit.py
@@ -4,12 +4,10 @@ class delimit:
     def __init__(self, delimiter):
         self.delimiter = delimiter
 
-    @staticmethod
-    def to_tuple(key):
+    def to_tuple(self, key):
         return tuple(key.split(self.delimiter))
 
-    @staticmethod
-    def from_tuple(obj):
+    def from_tuple(self, obj):
         return self.delimiter.join(obj)
 
 slash = delimit('/')
