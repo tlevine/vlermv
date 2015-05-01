b04a79de73c67eca7e7c5c314216b10c13527c23
Thomas Levine <_@thomaslevine.com>
2015-05-01 23:10:29 +0000

    -

--- vlermv/transformers/base64.py
+++ vlermv/transformers/base64.py
@@ -4,4 +4,6 @@ def to_path(path):
     return (b64encode(path.encode('utf-8')).decode('ascii'),)
 
 def from_path(key):
+    if len(key) != 1:
+        raise ValueError('Key must have exactly one element.')
     return b64decode(key[0]).decode('ascii')
---
+++ vlermv/transformers/test/test_simple.py
@@ -0,0 +1,12 @@
+import pytest
+
+from .. import simple
+
+def test_to_path():
+    assert simple.to_path('abc') == ('abc',)
+    with pytest.raises(ValueError):
+        simple.to_path('abc/def')
+
+def test_from_path():
+    with pytest.raises(ValueError):
+        simple.to_path(('abc', 'def'))
