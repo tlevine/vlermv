0d9d1ba2eb402f32e929424a4d2ed36a0a00ea90
Thomas Levine <_@thomaslevine.com>
2015-05-01 23:20:07 +0000

    -

--- vlermv/transformers/simple.py
+++ vlermv/transformers/simple.py
@@ -1,9 +1,14 @@
 import os
 
+error_msg = '''The default transformer cannot handle slashes (subdirectories);
+try another transformer in vlermv.transformers.'''
+
 def to_path(key):
-    if '/' in key or '\\' in key:
-        raise ValueError('The default transformer cannot handle slashes; try another transformer in vlermv.transformers.')
+    if '/' in key or '\\' in key or os.path.sep in key:
+        raise ValueError(error_msg)
     return (key,)
 
 def from_path(path):
-    return os.path.join(*path)
+    if len(path) != 1:
+        raise ValueError(error_msg)
+    return path[0]
