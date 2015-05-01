f5604bffd55a5cbe01d3b5d4f19a7079e40e1a19
Thomas Levine <_@thomaslevine.com>
2015-05-01 20:58:30 +0000

    yay

--- vlermv/_util.py
+++ vlermv/_util.py
@@ -53,5 +53,12 @@ def method_or_name(namespace, x):
 These attributes are available: %s.''' % (x, namespace.__name__, attrs)
     raise AttributeError(msg)
 
-def safe_path(unsafe_path):
-    return tuple(posixpath.normpath(posixpath.join('/', posixpath.join(*unsafe_path))).split('/')[1:])
+def safe_path(unsafe_path, not_allowed = {'', '.', '..'}):
+    unsafe_str = posixpath.join('', *unsafe_path)
+    rough_tuple = posixpath.normpath(posixpath.join('/', unsafe_str)).split('/')[1:]
+    safe_tuple = tuple(x for x in rough_tuple if x not in not_allowed)
+
+    if len(safe_tuple) > 0:
+        return safe_tuple
+    else:
+        raise ValueError('Path must not be empty.')
