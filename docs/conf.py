8cde0cd57eb9054f2a877e364c23d424e38ef537
Thomas Levine <_@thomaslevine.com>
2015-05-02 00:11:37 +0000

    theme

--- docs/conf.py
+++ docs/conf.py
@@ -48,7 +48,8 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+# html_theme = 'default'
+html_theme = 'dotted' #zzzeeksphinx' # bizstyle'
 
 # Themes in PyPI
 # hachibee-sphinx-theme guzzle_sphinx_theme sphinx_bernard_theme
