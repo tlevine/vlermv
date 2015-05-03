df2ebb3db10e5b0ae1986cea62f66e716174afd5
Thomas Levine <_@thomaslevine.com>
2015-05-03 17:02:19 +0000

    -

--- vlermv/transformers/_archive.py
+++ vlermv/transformers/_archive.py
@@ -2,21 +2,25 @@ import datetime
 from . import tuple
 
 def archive(transformer = tuple,
-            interval = 'day',
+            precision = 'day',
             position = 'left',
-            append_random = False):
+            now = datetime.date.now):
     '''
     Wrap a transformer to add a date component to a path.
-    '''
-    """
-Interval
-    millisecond, second, minute, hour, day, week, month, default is day
-append_random
-    Whether to append a random number after the date, just in case there
-    are multiple calls within the same millisecond, default is False
-position
-    left (at the beginning of the path), right (end), default is left
-    (should there also be a "replace" option? i think
 
-@cache(transformer = vlermv.transformers.archive(vlermv.transformers.tuple)
-    """
+    :param transformer: Transformer to be wrapped
+    :type transformer: transformer
+    :param str precision: How precise should the date component be?
+        Options are 'millisecond', 'second', 'minute', 'hour', 'day',
+        'week', and 'month'.
+    :param str position: Should the date be added to the beginning
+        of the path (:samp:`'left'`) or the end of the path
+        (:samp:`'right'`). If (:samp:`'left'`), the new path will be
+        :samp:`(date,) + old_path`; if right, the new path will be
+        :samp:`old_path + (date,)`.
+    :param function now: :py:func:`archive` will run this to determine
+        the date that will be added to the path; you might change
+        this for testing.
+    '''
+
+#@cache(transformer = vlermv.transformers.archive(vlermv.transformers.tuple)
