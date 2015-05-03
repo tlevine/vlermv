a6d317827a3ebc924a56d2ae6f0eb4111c750997
Thomas Levine <_@thomaslevine.com>
2015-05-03 19:32:59 +0000

    -

--- vlermv/transformers/test/test_archive.py
+++ vlermv/transformers/test/test_archive.py
@@ -5,11 +5,15 @@ import pytest
 from .. import ( tuple, archive )
 
 
-@pytest.mark.parametrize('position', [('left',), ('right',)])
+@pytest.mark.parametrize('position', ['left', 'right'])
 def test_position(position):
-    a = archive(transformer = tuple, date_format = 'DATE', position = position)
     key = (1, 2, 3)
-    observed = a.from_path(key)
+    class t:
+        @staticmethod
+        def to_path(_):
+            return key
+    a = archive(transformer = t, date_format = 'DATE', position = position)
+    observed = a.to_path(key)
     if position == 'left':
         assert observed[0] == 'DATE'
         assert observed[1:] == key
@@ -17,5 +21,7 @@ def test_position(position):
         assert observed[-1] == 'DATE'
         assert observed[:-1] == key
 
+def test_to_path():
+    'to_path should be the same as normal.'
 
 #@pytest.mark.parameterize('position, transformer, key', testcases_position):
