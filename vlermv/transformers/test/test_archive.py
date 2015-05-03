d991c568ef3373a00957f30fc69f5e55c2e1c8da
Thomas Levine <_@thomaslevine.com>
2015-05-03 19:30:03 +0000

    fix test

--- vlermv/transformers/test/test_archive.py
+++ vlermv/transformers/test/test_archive.py
@@ -8,20 +8,14 @@ from .. import ( tuple, archive )
 @pytest.mark.parametrize('position', [('left',), ('right',)])
 def test_position(position):
     a = archive(transformer = tuple, date_format = 'DATE', position = position)
-    @cache(transformer = a)
-    def f(a, b, c):
-        return a + b + c
     key = (1, 2, 3)
-    value = 6
-    f(*key)
-    assert f[key] == value
-    observed_value = next(f.keys())
+    observed = a.from_path(key)
     if position == 'left':
-        assert observed_value[0] == 'DATE'
-        assert observed_value[1:] == value
+        assert observed[0] == 'DATE'
+        assert observed[1:] == key
     elif position == 'left':
-        assert observed_value[-1] == 'DATE'
-        assert observed_value[:-1] == value
+        assert observed[-1] == 'DATE'
+        assert observed[:-1] == key
 
 
 #@pytest.mark.parameterize('position, transformer, key', testcases_position):
