e1f61f4d6f27da9ad1944fc7c2e5ec910d053f7e
Thomas Levine <_@thomaslevine.com>
2015-05-04 13:31:43 +0000

    int not allowed

--- vlermv/transformers/test/test_tuple.py
+++ vlermv/transformers/test/test_tuple.py
@@ -1,7 +1,13 @@
+import pytest
+
 from .. import tuple
 
 def test_to_path():
     assert tuple.to_path(('a',)) == ('a',)
+
+def test_to_path_int():
+    with pytest.raises(TypeError):
+        tuple.to_path((8,))
 
 def test_from_path():
     assert tuple.from_path(('a',)) == ('a',)
