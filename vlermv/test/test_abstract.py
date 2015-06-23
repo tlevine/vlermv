df72e23e26e34a18d5b667830819b8c35b842899
Thomas Levine <_@thomaslevine.com>
2015-06-23 10:18:34 +0000

    logging

--- vlermv/test/test_abstract.py
+++ vlermv/test/test_abstract.py
@@ -14,3 +14,17 @@ def test_logging():
 
     msg = 'Exception in AbstractVlermv calling this memoized function:\n<lambda>(*(8, 9, 3), *{})'
     l.check(('vlermv._abstract', 'ERROR', msg),)
+
+    assert False, print(l)
+
+def test_logging_cache_exceptions():
+
+    v = a.AbstractVlermv(cache_exceptions = True)
+    v.func = lambda *args: 1/0
+
+    with LogCapture() as l:
+        with pytest.raises(ZeroDivisionError):
+            v(8,9,3)
+
+    msg = 'Exception in AbstractVlermv calling this memoized function:\n<lambda>(*(8, 9, 3), *{})'
+    l.check(('vlermv._abstract', 'ERROR', msg),)
