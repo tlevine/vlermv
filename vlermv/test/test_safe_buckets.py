0479a5a64c1f41fea219d3d07d1352fa9f30aa55
Thomas Levine <_@thomaslevine.com>
2015-07-12 16:48:18 -0400

    testing safe buckets

--- vlermv/test/test_safe_buckets.py
+++ vlermv/test/test_safe_buckets.py
@@ -1,4 +1,24 @@
 import thready
+from .._safe_buckets import SafeBuckets
+
+class Count:
+    def __init__(self):
+        self._i = 0
+
+    @property
+    def i(self):
+        self._i += 1
+        return int(self._i)
+
+def create_bucket(bucketname, counter = Count()):
+    return (bucketname, counter.i)
+
+def test_create_bucket():
+    assert create_bucket('one') == ('one', 1)
+    assert create_bucket('two') == ('two', 2)
 
 def test_get_new():
     pass
+   #sb = SafeBuckets(create_bucket = create_bucket)
+   #assert sb['abc'] == ('abc', 1)
+   #assert sb['abc'] == ('abc', 1)
