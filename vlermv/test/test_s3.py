77e0aba3a0ecba48313cd192cb07b88f7446b91f
Thomas Levine <_@thomaslevine.com>
2015-07-12 16:19:09 -0400

    switch connect_s3 to bucket in tests

--- vlermv/test/test_s3.py
+++ vlermv/test/test_s3.py
@@ -4,37 +4,28 @@ import pytest
 
 from .._s3 import S3Vlermv, split
 
-class FakeS3:
-    def __init__(self, **kwargs):
-        self.db = kwargs
-    def __call__(self):
-        return self
-    def create_bucket(self, bucketname):
-        return FakeBucket(self)
-
 class FakeBucket:
-    def __init__(self, s3):
-        self.s3 = s3
+    def __init__(self, **db):
+        self.db = db
     def new_key(self, key):
-        return FakeKey(self.s3, key)
+        return FakeKey(self.db, key)
     def get_key(self, key):
-        if key in self.s3.db:
+        if key in self.db:
             return FakeKey(self.s3, key)
 
 class FakeKey:
-    def __init__(self, s3, key):
-        self.s3 = s3
+    def __init__(self, db, key):
         self.key = key
     def get_contents_as_string(self):
-        return self.s3.db[self.key]
+        return self.db[self.key]
     def get_contents_to_filename(self, filename):
         with open(filename, 'wb') as fp:
-            fp.write(self.s3.db[self.key])
+            fp.write(self.db[self.key])
     def set_contents_from_string(self, payload, **kwargs):
-        self.s3.db[self.key] = payload
+        self.db[self.key] = payload
     def set_contents_from_filename(self, filename, **kwargs):
         with open(filename, 'rb') as fp:
-            self.s3.db[self.key] = fp.read()
+            self.db[self.key] = fp.read()
 
 CONTRACT = {
     'bids': [],
@@ -47,15 +38,15 @@ PAYLOAD = json.dumps(CONTRACT).encode('utf-8')
 
 def test_read():
     d = S3Vlermv('contracts', serializer = json,
-                 connect_s3 = FakeS3(OP00032101 = PAYLOAD))
+                 bucket = FakeBucket(OP00032101 = PAYLOAD))
     assert d['OP00032101'] == CONTRACT
 
 def test_write():
-    fakes3 = FakeS3()
-    d = S3Vlermv('contracts', connect_s3 = fakes3, serializer = json)
-    assert fakes3.db == {}
+    fakebucket = FakeBucket()
+    d = S3Vlermv('contracts', bucket = fakebucket, serializer = json)
+    assert fakebucket.db == {}
     d['OP00032101'] = CONTRACT
-    assert fakes3.db == {'OP00032101': PAYLOAD}
+    assert fakebucket.db == {'OP00032101': PAYLOAD}
 
 def test_split():
     assert split('a/bb/cc') == ('a', 'bb', 'cc')
