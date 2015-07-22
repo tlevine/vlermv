8603a0295ba800e8776bc14a969cbba6a8911a51
Thomas Levine <_@thomaslevine.com>
2015-07-21 23:33:33 -0400

    requires

--- setup.py
+++ setup.py
@@ -6,11 +6,12 @@ setup(name='vlermv',
       description='Easily dump python objects to files, and then load them back.',
       url='https://thomaslevine.com/!/vlermv/',
       packages=['vlermv', 'vlermv.serializers', 'vlermv.transformers'],
-      install_requires = [],
-      tests_require = [
-          'pytest>=2.6.4', 'testfixtures>=4.1.2',
+      install_requires = [
           'thready>=1.4.0',
           'boto',
+      ],
+      tests_require = [
+          'pytest>=2.6.4', 'testfixtures>=4.1.2',
       ],
       version='1.3.4',
       license='LGPL',
