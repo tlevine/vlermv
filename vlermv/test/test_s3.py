267adf327a80e1e72cfe4fa9e8c5f7ae2a87f09c
Thomas Levine <_@thomaslevine.com>
2015-08-05 21:45:50 -0400

    test un-prefixing

--- vlermv/test/test_s3.py
+++ vlermv/test/test_s3.py
@@ -8,9 +8,10 @@ class FakeBucket:
     def __init__(self, name, **db):
         self.db = db
         self.name = name
-    def list(self):
+    def list(self, prefix = ''):
         for key in self.db:
-            yield self.new_key(key)
+            if key.startswith(prefix):
+                yield self.new_key(key)
     def new_key(self, key):
         return FakeKey(self.db, key)
     def get_key(self, key):
@@ -20,19 +21,19 @@ class FakeBucket:
         del(self.db[key])
 
 class FakeKey:
-    def __init__(self, db, key):
+    def __init__(self, db, name):
         self.db = db
-        self.key = key
+        self.name = name
     def get_contents_as_string(self):
-        return self.db[self.key]
+        return self.db[self.name]
     def get_contents_to_filename(self, filename):
         with open(filename, 'wb') as fp:
-            fp.write(self.db[self.key])
+            fp.write(self.db[self.name])
     def set_contents_from_string(self, payload, **kwargs):
-        self.db[self.key] = payload
+        self.db[self.name] = payload
     def set_contents_from_filename(self, filename, **kwargs):
         with open(filename, 'rb') as fp:
-            self.db[self.key] = fp.read()
+            self.db[self.name] = fp.read()
 
 CONTRACT = {
     'bids': [],
@@ -61,3 +62,10 @@ def test_delete():
     d['OP00032101'] = CONTRACT
     del(d['OP00032101'])
     assert len(fakebucket.db) == 0
+
+def test_prefix():
+    db = {'contracts/OP00032101': PAYLOAD}
+    d = S3Vlermv('procurement-documents', 'contracts', serializer = json,
+                 bucket = FakeBucket('procurement-documents', **db))
+    assert list(d.keys()) == [('OP00032101',)]
+    assert d['OP00032101'] == CONTRACT
