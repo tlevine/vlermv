f6bd64d6642f429ddf1e009212e47e39b0d852ca
Thomas Levine <_@thomaslevine.com>
2015-08-05 21:28:02 -0400

    revert

--- vlermv/_safe_buckets.py
+++ vlermv/_safe_buckets.py
@@ -2,31 +2,21 @@ from time import sleep
 import threading, queue
 
 from boto import connect_s3
-from boto.exception import S3CreateError
 
-def create_buckets(create_bucket, get_all_buckets, log, state):
+def create_buckets(create_bucket, log, state):
     while True:
         bucketname = log.get()
-        try:
-            state[bucketname] = create_bucket(bucketname)
-        except S3CreateError as e:
-            code, message, xml = e.args
-            wrong_region = b'Your previous request to create the named bucket succeeded and you already own it.'
-            if code == 409 and message == 'Conflict' and wrong_region in xml:
-                state[bucketname] = next(b for b in get_all_buckets() if b.name == bucketname)
-            else:
-                raise
+        state[bucketname] = create_bucket(bucketname)
 
 class SafeBuckets:
     '''
     Thread-safely create a bucket.
     '''
-    def __init__(self, create_bucket = connect_s3().create_bucket,
-                 get_all_buckets = connect_s3().get_all_buckets):
+    def __init__(self, create_bucket = connect_s3().create_bucket):
         self.log = queue.Queue()
         self.state = {}
         creator = threading.Thread(target = create_buckets,
-           args = (create_bucket, get_all_buckets, self.log, self.state), daemon = True)
+           args = (create_bucket, self.log, self.state), daemon = True)
         creator.start()
 
     def __getitem__(self, k):
