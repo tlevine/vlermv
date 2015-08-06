f6478776ac02f3ccd5431138be8150399d4ffbd4
Thomas Levine <_@thomaslevine.com>
2015-08-06 09:15:30 -0400

    fix the raw transformer

--- vlermv/transformers/raw.py
+++ vlermv/transformers/raw.py
@@ -3,7 +3,7 @@ import posixpath
 error_msg = '''The index must be a string.'''
 
 def to_path(key):
-    return (key,)
+    return tuple(key.strip('/').split('/'))
 
 def from_path(path):
     return posixpath.join(*path)
