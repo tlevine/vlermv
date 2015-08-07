c78cb26ff07712027b3ae340d6209482e8708641
Thomas Levine <_@thomaslevine.com>
2015-08-07 11:12:37 -0400

    implement single-length tuple

--- vlermv/transformers/raw.py
+++ vlermv/transformers/raw.py
@@ -3,7 +3,13 @@ import posixpath
 error_msg = '''The index must be a string.'''
 
 def to_path(key):
-    return tuple(key.strip('/').split('/'))
+    if isinstance(key, tuple) and len(key) == 1:
+        key = key[0]
+
+    if hasattr(key, 'strip') and hasattr(key, 'split'):
+        return tuple(key.strip('/').split('/'))
+    else:
+        raise TypeError('Key must be string-like or a tuple of length one.')
 
 def from_path(path):
     return posixpath.join(*path)
