ede2b51cc695045c30f2d9e3d91af6059a4659e2
Thomas Levine <_@thomaslevine.com>
2015-08-07 11:11:12 -0400

    test single tuples

--- vlermv/transformers/test/test_raw.py
+++ vlermv/transformers/test/test_raw.py
@@ -2,17 +2,30 @@ import pytest
 
 from .. import raw
 
-testcases = [
+testcases_str = [
     ('abc', ('abc',)),
     ('abc/def', ('abc', 'def')),
     ('abc/def/', ('abc', 'def')),
     ('/abc/def/', ('abc', 'def')),
 ]
 
-@pytest.mark.parametrize('left, right', testcases)
+@pytest.mark.parametrize('left, right', testcases_str)
 def test_to_path(left, right):
     assert raw.to_path(left) == right
 
-@pytest.mark.parametrize('left, right', testcases)
+@pytest.mark.parametrize('left, right', testcases_str)
 def test_from_path(left, right):
     assert left.strip('/') == raw.from_path(right)
+
+testcases_tuple = [
+    (('abc/def',), ('abc', 'def')),
+    (('abc', 'def'), None),
+]
+
+@pytest.mark.parametrize('left, right', testcases_tuple)
+def test_tuple(left, right):
+    if right == None:
+        with pytest.raises(TypeError):
+            raw.to_path(left)
+    else:
+        assert raw.to_path(left) == right
